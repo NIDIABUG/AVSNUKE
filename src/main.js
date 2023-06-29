079aa27c6fee7e5a56e1a35b2a19ff77b31f87ea
NIDIABUG <nidiastudioph@gmail.com>
2023-06-29 21:53:36 +0800

    -50% Spaghettified code gone.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+// Import stuffs
 require('dotenv').config();
 const { Client, IntentsBitField, ActivityType, SnowflakeUtil } = require('discord.js');
 
+// Make a Bot instance with this attibutes
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -10,6 +12,7 @@ const client = new Client({
     ],
 });
 
+// Log when the client (Bot) is connected
 client.on('ready', (c) => {
     console.log(`${c.user.tag} is Online`);
 
@@ -18,57 +21,42 @@ client.on('ready', (c) => {
     });
 });
 
+// All of the command handlers
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) { return; }
+
     else if (interaction.commandName === 'hey') {
         interaction.reply('hello ' + interaction.user.username);
     }
     else if (interaction.commandName === 'nuke') {
         const radius = interaction.options.get('radius').value;
-        var last_message = interaction.options.get('last-message')?.value;
+        var last_message = interaction.options.getString('last-message');
 
-        // no message
-        if (last_message === undefined) {
-            last_message = 'with no messages';
-        } else {
-            last_message = 'saying \'' + last_message + '\'';
-        }
-
+        last_message  == undefined ? last_message = 'with no messages' : last_message = 'saying \'' + last_message + '\'';
         interaction.reply(interaction.user.username + ' activated a nuke with a radius of ' + radius + ' ' + last_message);
     }
     else if (interaction.commandName == 'calculate') {
+        // Thinking about using the getNumber() function, but It concatenates the number using the '+' operator like a string lmao
         const numberOne = interaction.options.get('number-one').value;
         const numberTwo = interaction.options.get('number-two').value;
-        const operator = interaction.options.get('operator').value;
-        let sum;
-        if (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/') {
-            interaction.reply(interaction.user.username + 'wrong command, use (+, -, *, /) for the operator');
-            return;
-        } else {
-            switch (operator) {
-                case '+':
-                    sum = numberOne + numberTwo;
-                    break;
-                case '-':
-                    sum = numberOne - numberTwo;
-                    break;
-                case '*':
-                    sum = numberOne * numberTwo;
-                    break;
-                case '/':
-                    sum = numberOne / numberTwo;
-                    break;
-                default:
-                    break;
-            }
-        }
+        const operator = interaction.options.getString('operator');
+        let sum = (() => {
+            if (operator === '+') return numberOne + numberTwo;
+            else if (operator === '-') return numberOne - numberTwo;
+            else if (operator === '*') return numberOne * numberTwo;
+            else if (operator === '/') return numberOne / numberTwo;
+        })();
+        if (!sum) return interaction.reply(interaction.user.username + ' wrong command, use (+, -, *, /) for the operator');
         interaction.reply(interaction.user.username + ' ' + numberOne + ' ' + operator + ' ' + numberTwo + ' is equal to ' + sum);
     }
 });
 
+// Mics stuff for testing
 client.on('messageCreate', (msg) => {
     if (msg.content === "eyo AVSNUKE add this 5 + 10") {
         msg.reply('its equal to 15 homie');
     }
 });
+
+// Clients Token
 client.login(process.env.TOKEN);
