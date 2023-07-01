d877927b18e529d54567c9d2f66d873bdc5a5823
NIDIABUG <nidiastudioph@gmail.com>
2023-07-02 07:13:27 +0800

    added '!delete' to delete message

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ client.on('interactionCreate', (interaction) => {
         const radius = interaction.options.get('radius').value;
         var last_message = interaction.options.getString('last-message');
 
-        last_message  == undefined ? last_message = 'with no messages' : last_message = 'saying \'' + last_message + '\'';
+        last_message == undefined ? last_message = 'with no messages' : last_message = 'saying \'' + last_message + '\'';
         interaction.reply(interaction.user.username + ' activated a nuke with a radius of ' + radius + ' ' + last_message);
     }
     else if (interaction.commandName == 'calculate') {
@@ -52,9 +52,22 @@ client.on('interactionCreate', (interaction) => {
 });
 
 // Mics stuff for testing
-client.on('messageCreate', (msg) => {
-    if (msg.content === "eyo AVSNUKE add this 5 + 10") {
-        msg.reply('its equal to 15 homie');
+client.on('messageCreate', async (message) => {
+    if (message.content === "eyo AVSNUKE add this 5 + 10") {
+        message.reply('its equal to 15 homie');
+    }
+    else if (message.content === '!delete') {
+        try{
+            const fethchMessage = await message.channel.messages.fetch({limit: 10});
+            message.channel.bulkDelete(fethchMessage)
+            .then((deletedMessages) => {
+                const deleteCount = deletedMessages.size;
+                console.log('Deleted ${deleteCount} messages');
+            })
+            .catch((err) => console.error('error deleting messages', err));
+        }catch (e) {
+            console.error('Error fetching messages:', e);
+        }
     }
 });
 
